e7e5367480d5fda9b743fc7cbddd405eb8376ccf
oxau <notoriousact@gmail.com>
2023-02-06 23:39:27 -0800

    xoxo

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
+    male = int(input('Number of Males registered in the class: '))
+    female = int(input('Number of Females registered in the class: '))
+    total = int((male) + (female))
+    malepercent = float(male/total)
+    femalepercent = float(female/total)
+
+    print ('The total number of students: ' + '\t\t{0}'. format(total))
+    print ('The numbers of males and females: ' + '\t{0} \t\t{1}'. format (male, female))
+    print ('The percentage of males and females: ' + '\t{0:.2f}% \t\t{1:.2f}%'. format(malepercent*100, femalepercent*100))
     pass
 
 
